24a671563d8185d812ddd1ce7b072b6f9eabbe25
Илья Закатимов <ilya_zakatimov@MacBook-Pro-Ila.local>
2021-10-04 23:25:30 +0300

    Initial commit

---
+++ loadermanger/src/main/java/ru/mirea/petukhov/loadermanger/MyLoader.java
@@ -0,0 +1,40 @@
+package ru.mirea.petukhov.loadermanger;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import androidx.annotation.NonNull;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class MyLoader extends AsyncTaskLoader<String> {
+    private String firstName;
+    public static final String ARG_WORD = "word";
+    public MyLoader(@NonNull Context context, Bundle args) {
+        super(context);
+        if (args != null){
+            firstName = args.getString(ARG_WORD);
+            char[] words = firstName.toCharArray();
+            for (int ind = 0;  ind < words.length; ++ind){
+                int newInd = getRandomNumber(0, words.length);
+                char symbolSave = words[newInd];
+                words[newInd] = words[ind];
+                words[ind] = symbolSave;
+            }
+            firstName = String.copyValueOf(words);
+        }
+    }
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+    @Override
+    public String loadInBackground() {
+        SystemClock.sleep(5000);
+        return firstName;
+    }
+}
